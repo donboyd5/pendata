2ca2f5d7591f14fd6de5ccab11c342220e2f3d8a
Don Boyd <donboyd5@gmail.com>
2024-12-10 13:24:45 -0500

    update frs data creation

---
+++ R/frs.R
@@ -0,0 +1,15 @@
+#' FRS Florida Retirement System
+#'
+#' A list with data for the Florida Retirement System.
+#'
+#' @format ## `frs`
+#' A list:
+#' \describe{
+#'   \item{inputs}{list with stacked data frames for entrant profiles, motality, headcount and salary, salary growth, separation rates, workforce, and various scalars}
+#'   \item{default_params}{list with default parameters used by Reason in building the model; to be winnowed down}
+#' }
+#' @source
+#' Actuarial valuation 2022, ACFR 2022, and SOA. As prepared by Reason team.
+"frs"
+
+
