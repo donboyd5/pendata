b80a5f371af347b3ca10b2e6d904dd73ce1bfa3e
Don Boyd <donboyd5@gmail.com>
2025-07-09 10:39:47 -0400

    delete old folders file

--- R/folders.R
+++
@@ -1,6 +0,0 @@
-# define paths to folders we'll always want access to,
-# whether in the extract_data subproject or the prepare_data subproject
-
-DXDATA <- here::here("extract_data")
-DPREP <- here::here("prepare_data")
-DPLANS <- here::here("plans")
