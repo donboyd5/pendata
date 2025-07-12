22803fb0e7db548b5373b6f824a20c27e37c67c6
Don Boyd <donboyd5@gmail.com>
2025-07-12 07:13:41 -0400

    reorganize folders

--- data-raw/R/folders_function.R
+++ data-raw/R/folders_function.R
@@ -1,28 +1,3 @@
-# set_plan_dirs <- function(PLAN_CONSTANTS) {
-#   plans <- here::here("data-raw", "plans") # ALL data for specific plans is here, other than final package data
-#   list(
-#     # top-level folders for all plans
-#     extract_data = here::here("data-raw", "extract_data"), # generic quarto project for extracting data
-#     prepare_data = here::here("data-raw", "prepare_data"), # holds plan-specific folders for preparing extracted data
-
-#     # plan-specific folders
-#     plandir = fs::path(plans, PLAN_CONSTANTS$plan),
-#     xddir = fs::path(plans, PLAN_CONSTANTS$plan, "extracted_data"),
-#     plan_xdfile = fs::path(
-#       plans,
-#       PLAN_CONSTANTS$plan,
-#       "extracted_data",
-#       PLAN_CONSTANTS$xd_filename
-#     ),
-#     work = fs::path(plans, PLAN_CONSTANTS$plan, "work"),
-#     staged = fs::path(plans, PLAN_CONSTANTS$plan, "staged"),
-#     prep_code = fs::path(
-#       here::here("data-raw", "prepare_data"),
-#       PLAN_CONSTANTS$plan
-#     )
-#   )
-# }
-
 set_plan_dirs <- function(plan_constants) {
   # Base directories
   data_raw <- here::here("data-raw")
@@ -33,10 +8,8 @@ set_plan_dirs <- function(plan_constants) {
   list(
     # Top-level folders
     extract_data = fs::path(data_raw, "extract_data"),
-    prepare_data = fs::path(data_raw, "prepare_data"),
 
     # Plan-specific folders
-    plandir = plan_dir,
     xddir = fs::path(plan_dir, "extracted_data"),
     plan_xdfile = fs::path(
       plan_dir,
--- data-raw/R/setup.R
+++ data-raw/R/setup.R
@@ -4,5 +4,6 @@ source(here::here("data-raw", "R", "libraries.R"))
 source(here::here("data-raw", "R", "constants.R"))
 source(here::here("data-raw", "R", "folders_function.R"))
 source(here::here("data-raw", "R", "functions.R"))
+source(here::here("data-raw", "R", "functions_data.R"))
 
 PLAN_CONSTANTS <- jsonlite::fromJSON(here::here("data-raw", "plan_config.json"))
