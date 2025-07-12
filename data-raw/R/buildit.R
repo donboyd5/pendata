355c1158288f0dd656c3386bef0dca0f1e1bf3df
Don Boyd <donboyd5@gmail.com>
2025-07-12 06:10:50 -0400

    improve buildit script

--- data-raw/R/buildit.R
+++ data-raw/R/buildit.R
@@ -1,8 +1,23 @@
-# close any externally open files (e.g., Excel) that are in project folders
-detach(package:pendata)
-remove.packages("pendata")
+# before running, close any externally open files (e.g., Excel) that are in project folders
+
+# Clear existing package if loaded
+if ("package:pendata" %in% search()) {
+  detach("package:pendata", unload = TRUE, force = TRUE)
+}
+
+# Remove installed package
+try(remove.packages("pendata"), silent = TRUE)
+
+# Clear objects from memory
+rm(list = ls(all.names = TRUE)) # Clears all objects
+gc() # Triggers garbage collection
+
+# Rebuild and install
 devtools::document()
-# devtools::build() # not needed when doing devtools::install()
 devtools::install()
+
+# Load the fresh package
 library(pendata)
+packageVersion("pendata")
+
 plan <- pendata::frs
