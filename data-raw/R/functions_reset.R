518b9edc584727611eff1896c56c33abb8292256
Don Boyd <donboyd5@gmail.com>
2025-07-12 17:25:23 -0400

    improve reset_all

--- data-raw/R/functions_reset.R
+++ data-raw/R/functions_reset.R
@@ -1,8 +1,57 @@
-reset_all <- function(plan) {
+reset_all <- function(plan, delete = TRUE) {
+  data <- here::here("data")
   plan_dir <- here::here("data-raw", "plans", plan)
   work <- fs::path(plan_dir, "work")
   staged <- fs::path(plan_dir, "staged")
-  return(c(work, staged))
+
+  # Data file path correction
+  if (!fs::dir_exists(data)) {
+    message(sprintf("Data directory does not exist: %s", data))
+    data_files <- character(0)
+  } else {
+    candidate <- fs::path(data, paste0(plan, ".rda"))
+    if (fs::file_exists(candidate)) {
+      data_files <- candidate
+    } else {
+      data_files <- character(0)
+    }
+  }
+
+  if (!fs::dir_exists(work)) {
+    message(sprintf("Work directory does not exist: %s", work))
+    work_files <- character(0)
+  } else {
+    work_rds_files <- fs::dir_ls(path = work, glob = "*.rds")
+    work_csv_files <- fs::dir_ls(path = work, glob = "*.csv")
+    work_files <- c(work_rds_files, work_csv_files)
+  }
+
+  if (!fs::dir_exists(staged)) {
+    message(sprintf("Staged directory does not exist: %s", staged))
+    staged_files <- character(0)
+  } else {
+    staged_files <- fs::dir_ls(path = staged, glob = "*.rds")
+  }
+
+  files <- c(data_files, work_files, staged_files)
+
+  if (length(files) == 0) {
+    message("No files found to delete.")
+  } else if (delete) {
+    purrr::walk(files, function(file) {
+      if (fs::file_exists(file)) {
+        fs::file_delete(file)
+        message(sprintf("Deleted: %s", file))
+      }
+    })
+  } else {
+    message("Files available to delete:")
+    purrr::walk(files, message)
+  }
+
+  invisible(NULL)
 }
 
-reset_all("frs")
+# Example usage
+# reset_all("frs", delete = FALSE)
+# reset_all("frs")
--- data-raw/R/setup.R
+++ data-raw/R/setup.R
@@ -2,7 +2,8 @@
 
 source(here::here("data-raw", "R", "libraries.R"))
 source(here::here("data-raw", "R", "constants.R"))
-source(here::here("data-raw", "R", "function_folders.R"))
 source(here::here("data-raw", "R", "functions.R"))
+source(here::here("data-raw", "R", "functions_folders.R"))
+source(here::here("data-raw", "R", "functions_reset.R"))
 
 PLAN_CONSTANTS <- jsonlite::fromJSON(here::here("data-raw", "plan_config.json"))
