eb80b9e5f8c29c88e6f281f594948178d5ba237a
Don Boyd <donboyd5@gmail.com>
2025-07-13 07:31:42 -0400

    limit set_plan_dirs() to folders

--- data-raw/R/functions_folders.R
+++ data-raw/R/functions_folders.R
@@ -1,23 +1,27 @@
-set_plan_dirs <- function(plan_constants) {
+set_plan_dirs <- function(plan) {
   # Base directories
+  data <- here::here("data")
   data_raw <- here::here("data-raw")
   plans_dir <- fs::path(data_raw, "plans")
-  plan_dir <- fs::path(plans_dir, plan_constants$plan)
+  plan_dir <- fs::path(plans_dir, plan)
 
   # Return named list
   list(
     # Top-level folders
+    data = data,
     extract_data = fs::path(data_raw, "extract_data"),
 
     # Plan-specific folders
+    plan_dir = plan_dir,
     xddir = fs::path(plan_dir, "extracted_data"),
-    plan_xdfile = fs::path(
-      plan_dir,
-      "extracted_data",
-      plan_constants$xd_filename
-    ),
-    work = fs::path(plan_dir, "work"),
-    staged = fs::path(plan_dir, "staged"),
-    prep_code = fs::path(data_raw, "prepare_data", plan_constants$plan)
+    work = fs::path(plan_dir, "work_data"),
+    staged = fs::path(plan_dir, "staged_data"),
+    prep_code = fs::path(data_raw, "prepare_data", plan)
   )
 }
+
+# plan_xdfile = fs::path(
+#   plan_dir,
+#   "extracted_data",
+#   plan_constants$xd_filename
+# ),
