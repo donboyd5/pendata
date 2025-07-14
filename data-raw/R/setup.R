2a69d43b59f137274c7bad8fc6d3112a49e1e839
Don Boyd <donboyd5@gmail.com>
2025-07-14 16:24:40 -0400

    minor edits

--- data-raw/R/setup.R
+++ data-raw/R/setup.R
@@ -3,8 +3,8 @@
 source(here::here("data-raw", "R", "libraries.R"))
 source(here::here("data-raw", "R", "constants.R"))
 source(here::here("data-raw", "R", "functions.R"))
-source(here::here("data-raw", "R", "functions_folders.R"))
 source(here::here("data-raw", "R", "functions_reset.R"))
+source(here::here("data-raw", "R", "functions_folders.R"))
 source(here::here("data-raw", "R", "buildit.R"))
 
 PLAN_CONSTANTS <- jsonlite::fromJSON(here::here("data-raw", "plan_config.json"))
