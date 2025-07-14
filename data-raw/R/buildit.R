ffa6790c32049855fcf27481803cc9edcc6df5d4
Don Boyd <donboyd5@gmail.com>
2025-07-14 05:53:57 -0400

    create buildit function

--- data-raw/R/buildit.R
+++ data-raw/R/buildit.R
@@ -1,23 +1,25 @@
-# before running, close any externally open files (e.g., Excel) that are in project folders
+buildit <- function() {
+  # before running, close any externally open files (e.g., Excel) that are in project folders
 
-# Clear existing package if loaded
-if ("package:pendata" %in% search()) {
-  detach("package:pendata", unload = TRUE, force = TRUE)
+  # Clear existing package if loaded
+  if ("package:pendata" %in% search()) {
+    detach("package:pendata", unload = TRUE, force = TRUE)
+  }
+
+  # Remove installed package
+  try(remove.packages("pendata"), silent = TRUE)
+
+  # Clear objects from memory
+  rm(list = ls(all.names = TRUE)) # Clears all objects
+  gc() # Triggers garbage collection
+
+  # Rebuild and install
+  devtools::document()
+  devtools::install()
+
+  # Load the fresh package
+  library(pendata)
+  packageVersion("pendata")
 }
 
-# Remove installed package
-try(remove.packages("pendata"), silent = TRUE)
-
-# Clear objects from memory
-rm(list = ls(all.names = TRUE)) # Clears all objects
-gc() # Triggers garbage collection
-
-# Rebuild and install
-devtools::document()
-devtools::install()
-
-# Load the fresh package
-library(pendata)
-packageVersion("pendata")
-
-plan <- pendata::frs
+# plan <- pendata::frs
