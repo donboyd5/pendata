29f2cb2a5b448a24b7cf3c6a2088aaa2d8852383
Don Boyd <donboyd5@gmail.com>
2025-07-15 07:33:14 -0400

    delete obsolete folder names and update others

--- data-raw/R/functions_folders.R
+++ data-raw/R/functions_folders.R
@@ -2,26 +2,19 @@ set_plan_dirs <- function(plan) {
   # Base directories
   data <- here::here("data")
   data_raw <- here::here("data-raw")
-  plans_dir <- fs::path(data_raw, "plans")
   plan_dir <- fs::path(plans_dir, plan)
 
   # Return named list
   list(
     # Top-level folders
     data = data,
-    extract_data = fs::path(data_raw, "extract_data"),
+    data_raw = data_raw,
+    all_plans_dir = fs::path(data_raw, "plans"),
 
     # Plan-specific folders
     plan_dir = plan_dir,
     xddir = fs::path(plan_dir, "extracted_data"),
     work = fs::path(plan_dir, "work_data"),
-    staged = fs::path(plan_dir, "staged_data"),
-    prep_code = fs::path(data_raw, "prepare_data", plan)
+    staged = fs::path(plan_dir, "staged_data")
   )
 }
-
-# plan_xdfile = fs::path(
-#   plan_dir,
-#   "extracted_data",
-#   plan_constants$xd_filename
-# ),
