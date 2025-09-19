a7585d5429f08da6e795a0bac6a5213a57d44fcf
Don Boyd <donboyd5@gmail.com>
2025-09-19 11:02:34 -0400

    load excel functions

--- data-raw/R/setup.R
+++ data-raw/R/setup.R
@@ -4,6 +4,7 @@
 source(fs::path(GROOT, "data-raw", "R", "libraries.R"))
 source(fs::path(GROOT, "data-raw", "R", "constants.R"))
 source(fs::path(GROOT, "data-raw", "R", "functions.R"))
+source(fs::path(GROOT, "data-raw", "R", "functions_get_excel_data.R"))
 source(fs::path(GROOT, "data-raw", "R", "functions_folders.R"))
 
 PLAN_CONSTANTS <- jsonlite::fromJSON(fs::path(
